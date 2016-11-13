c518c00d417b8d080980a723dc42910b8e4ca0c1
arthyom <allpipiasaaa@hotmail.com>
2016-11-13 14:16:31 -0600

    Cargando las imagenes Bmp

--- redNeuronal/Form1.cs
+++ redNeuronal/Form1.cs
@@ -12,15 +12,53 @@ namespace redNeuronal
 {
     public partial class Form1 : Form
     {
-        public Red RedNeuronalActual;
+        public Red      RedNeuronalActual;
+        public String   RutaGeneralImagenes = "../Imagenes/4's/";
 
         public Form1()
         {
             InitializeComponent();
         }
 
+
+        void CargarBmpFromFile( string rutaEntrada )
+        {
+            // leer el bmp desde el archivo de texto indicado 
+            Bitmap imagenEntrada = new Bitmap(rutaEntrada, true);
+
+            // crear una matriz para guardar los pixeles de la matriz
+            int [,] matrizNumerica  = new int[imagenEntrada.Height, imagenEntrada.Height];
+
+            // leer la matriz de entrada y mover los unos o ceros 
+            for (int i = 0; i < imagenEntrada.Height; i++)
+            {
+                for (int j = 0; j < imagenEntrada.Width; j++)
+                {
+                    Color verificador = imagenEntrada.GetPixel(j, i);
+
+                    if ( verificador.ToArgb() == Color.Black.ToArgb() ) 
+                        matrizNumerica[i, j] = 1;
+                    else
+                        matrizNumerica[i, j] = 0;
+                }
+            }
+
+            // mostrar la matriz 
+            for (int i = 0; i < imagenEntrada.Height; i++)
+            {
+                for (int j = 0; j < imagenEntrada.Width; j++)
+                    Console.Write( matrizNumerica[i,j]);
+                Console.WriteLine(" ");
+            }
+
+        }
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            //cargar la imagen del 4
+            CargarBmpFromFile(@"C:\Users\Frodo\documents\visual studio 2012\Projects\redNeuronal\redNeuronal\Imagenes\4's\4.1.bmp");
+
             // crear una nueva red neuronal 
             this.RedNeuronalActual = new Red("red actual");
 
