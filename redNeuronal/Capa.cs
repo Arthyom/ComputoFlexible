f23de2803abd91bfe3a306a16a84c326e0910b55
arthyom <allpipiasaaa@hotmail.com>
2016-11-12 21:19:04 -0600

    agregando la clase capa

--- redNeuronal/Capa.cs
+++ redNeuronal/Capa.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace redNeuronal
 {
     public class Capa
     {
+
+        /************************************************************************************
+                              propiedades publicas de la clase capa
+        ************************************************************************************/
+        public      int         numeroNeuronas;
+        public      string      nombreRed;
+        public      int         idNeurona;
+        public      ArrayList   neuronas;
+
+
+
+        /************************************************************************************
+                           metodos publicas de la clase neurona 
+        ************************************************************************************/
+        public Capa()
+        {
+            this.numeroNeuronas = 0;
+            this.nombreRed = "";
+            this.idNeurona = -1;     
+        }
     }
 }
